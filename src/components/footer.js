cb6713147172c20a265f8fb1cb677c2be96584d5
Akiba So <37971883+akibaso@users.noreply.github.com>
2021-11-14 18:19:17 +0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,8 +8,7 @@ const Footer = () => {
             <a href="https://2890.ltd/"><img src="https://cdn.hyh.ink/imger/2021/11/14/20211107bigicon6deca5a01c365e38.png" style={{height: '122px'}}/></a>
          <p><br/>Then stirs the feeling infinite, so felt.<br/>In solitude, where we are least alone.</p>
         <p>&copy; 2021 <Link to="https://2890.ltd/" text="OkYes Blog" /></p>
-        </br>
-         <p>Our Sponsors</p>
+<p><br/>Our Sponsors</p>
 <div><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3031963600874401"
      crossorigin="anonymous"></script>
 <!-- Status -->
