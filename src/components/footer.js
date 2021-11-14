377d96a52145e244fa8a392c62260b6086075ae8
Akiba So <37971883+akibaso@users.noreply.github.com>
2021-11-14 15:45:22 +0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div id="footer">
       <div className="container">
-            <a href="https://2890.ltd/"><img src="https://cdn.hyh.ink/imger/2021/11/14/20211107bigicon6deca5a01c365e38.png" style={{height: 200em}}/></a>
+            <a href="https://2890.ltd/"><img src="https://cdn.hyh.ink/imger/2021/11/14/20211107bigicon6deca5a01c365e38.png" style="{{height: 200em}}"/></a>
         <p>&copy; 2021 <Link to="https://2890.ltd/" text="OkYes Blog" /></p>
       </div>
     </div>
