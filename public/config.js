4b1bca69c2d845cb3462ef65976afaba94b2c21d
Akiba So <37971883+akibaso@users.noreply.github.com>
2021-11-14 13:31:51 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // 站点名
-  SiteName: 'Uptime Status',
+  SiteName: 'Akiba's Status',
 
   // 站点链接
   SiteUrl: '/',
@@ -11,15 +11,15 @@ window.Config = {
   // 只需填写域名部分，默认为官网域名
   // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
   // 详见 https://github.com/yb/uptime-status/ 说明
-  ApiDomain: 'api.uptimerobot.com',
+  ApiDomain: 'cool-leaf-fd6e.akiba.workers.dev',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm784345383-76da07daefb1dd03b06f1fb9',
+    'm789722373-6f4c7f7481bfc6a2bfe94e30',
+    'm784346520-8e99a7b1bbc32587e1060472',
+    'm785509380-0499622d9e20641289aad82f'
   ],
 
   // 是否显示监测站点的链接
@@ -28,17 +28,17 @@ window.Config = {
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 60,
+  CountDays: 90,
 
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: 'Blog',
+      url: 'https://2890.ltd/'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      text: 'Homepage',
+      url: 'https://hyh.ink/'
     }
   ]
 };
