730fe00d761e9ac0d073f2f477a37514e322bfeb
Akiba So <37971883+akibaso@users.noreply.github.com>
2021-11-14 14:06:08 +0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <div id="footer">
       <div className="container">
-        <p>基于 <Link to="https://uptimerobot.com/" text="UptimeRobot" /> 接口制作，检测频率 5 分钟</p>
-        <p>&copy; 2020 <Link to="https://status.org.cn/" text="STATUS.ORG.CN" /></p>
+        <p><img src="https://cdn.hyh.ink/imger/2021/11/14/20211107bigicon6deca5a01c365e38.png" style="height:200px;"/></p>
+        <p>&copy; 2021 <Link to="https://2890.ltd/" text="OkYes Blog" /></p>
       </div>
     </div>
   );
