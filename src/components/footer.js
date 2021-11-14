7aa0d7e3b1ca65e09b47815680f6222927a239c0
Akiba So <37971883+akibaso@users.noreply.github.com>
2021-11-14 18:05:47 +0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <div id="footer">
       <div className="container">
             <a href="https://2890.ltd/"><img src="https://cdn.hyh.ink/imger/2021/11/14/20211107bigicon6deca5a01c365e38.png" style={{height: '122px'}}/></a>
-         <p><br>Then stirs the feeling infinite, so felt.<br>In solitude, where we are least alone.</p>
+         <p><br/>Then stirs the feeling infinite, so felt.<br/>In solitude, where we are least alone.</p>
         <p>&copy; 2021 <Link to="https://2890.ltd/" text="OkYes Blog" /></p>
       </div>
     </div>
