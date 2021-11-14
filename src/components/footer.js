9b1d84a78c44e1c140cea88f6fc64191ca99b824
Akiba So <37971883+akibaso@users.noreply.github.com>
2021-11-14 14:50:00 +0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,6 @@ const Footer = () => {
   return (
     <div id="footer">
       <div className="container">
-        <p><img src="https://cdn.hyh.ink/imger/2021/11/14/20211107bigicon6deca5a01c365e38.png" style="height:200px;"/></p>
         <p>&copy; 2021 <Link to="https://2890.ltd/" text="OkYes Blog" /></p>
       </div>
     </div>
