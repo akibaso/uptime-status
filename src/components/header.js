3f936308228e21e3aa0a3c47a75f0cef9b1f205e
Akiba So <37971883+akibaso@users.noreply.github.com>
2021-11-14 17:04:06 +0800

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <div id="header">
       <div className="container">
         <img src="https://cdn.hyh.ink/tc/logo2890.svg" style={{height:'55px',position:'fixed'}}/>
-         <p style={{marginLeft:'66px',color:'#66ccff',marginTop:'5px'}}/>{SiteName}</p>
+<a className="logo" style={{marginLeft:'66px',color:'#66ccff',marginTop:'5px'}} href={SiteUrl}>{SiteName}</a>
         <div className="navi">
           {Navi.map((item, index) => (
             <Link key={index} text={item.text} to={item.url} />
